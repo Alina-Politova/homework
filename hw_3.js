c9cf93bb175370614a01a91a1a71b27f73c19ac5
Alina <alina.politova79@gmail.com>
2022-11-06 15:59:40 +0200

    homework

--- hw_3.js
+++ hw_3.js
@@ -1,17 +1,17 @@
 // HOMEWORK_3
 
-// Task_1
+Task_1
 
-// const arr = [2, 3, 4, 5];
-// let count = 1;
-// for (let p of arr) {
-//     count *= p;
-// }
-// console.log(count);
+const arr = [2, 3, 4, 5];
+let count = 1;
+for (let p of arr) {
+    count *= p;
+}
+console.log(count);
 
-// let i = 0;
-// let result = 1;
-// while ()
+let i = 0;
+let result = 1;
+while ()
 
 
 // Task_2
